d9ae4d05ab5ae78127ae48b886d47b5d7387d72a
jocnunez <jose.nunnez@gmail.com>
2024-01-19 20:48:23 +0100

    add get

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,23 @@
 console.log('Hola Node')
+
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+const tareas = [
+    "tarea1",
+    "tarea2",
+    "tarea3"
+];
+
+fastify.get('/', function (request, reply) {
+    reply.send({ hello: 'world' })
+})
+
+fastify.get('/tareas', function (request, reply) {
+    reply.send({ 'tareas': tareas })
+})
+
+
+fastify.listen({ port: 3000 })
