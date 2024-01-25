5d985004c9de3c9f6760cd9f8c930c92f8e47c76
jocnunez <jose.nunnez@gmail.com>
2024-01-25 20:45:12 +0100

    Co-authored-by: FMFE2 <FMFE2@users.noreply.github.com>

--- api/src/index.js
+++ api/src/index.js
@@ -1,23 +1,17 @@
-console.log('Hola Node')
-
+// Fastify es una librería para escribir api rest
 import Fastify from 'fastify'
+
+// Inicializando la constante fastify
 const fastify = Fastify({
-  logger: true
+    logger: true
 })
 
-const tareas = [
-    "tarea1",
-    "tarea2",
-    "tarea3"
-];
-
+// Definimos el metodo read de la "/"
 fastify.get('/', function (request, reply) {
     reply.send({ hello: 'world' })
 })
 
-fastify.get('/tareas', function (request, reply) {
-    reply.send({ 'tareas': tareas })
-})
+// TODO: Crear los métodos para completar el CRUD
 
-
+// Escucha y ejecuta la aplicación
 fastify.listen({ port: 3000 })
